fda581cb6abe1af540e3ea0519299b1259bd8c97
tarunreddy-26 <2311cs010496@mallareddyuniversity.ac.in>
2024-08-17 11:30:36 +0530

    Add files via upload

--- DynamicArray.java
+++ DynamicArray.java
@@ -1,87 +1,27 @@
-package dynamicarray;
-class Array {
-    private int[] items;
-    private int count;
-    public Array(int length) {
-        items = new int[length];
-    }
-    public void insert(int item) {
-        resizeIfRequired();
-        items[count++] = item;
-    }
-    public void insertAt(int item, int index) {
-        if (index < 0 || index > count) {
-            throw new IllegalArgumentException();
-        }
-        resizeIfRequired();
-        for (int i = count - 1; i >= index; i--) {
-            items[i + 1] = items[i];
-        }
-        items[index] = item;
-        count++;
-    }
-    private void resizeIfRequired() {
-        if (items.length == count) {
-            int[] newItems = new int[count * 2];
-            for (int i = 0; i < count; i++) {
-                newItems[i] = items[i];
-            }
-            items = newItems;
-        }
-    }
-    public void reverse() {
-        int[] newItems = new int[count];
-        for (int i = 0; i < count; i++) {
-            newItems[i] = items[count - i - 1];
-        }
-        items = newItems;
-    }
-    public int max() {
-        int max = 0;
-        for (int item : items) {
-            if (item > max) {
-                max = item;
-            }
-        }
-        return max;
-    }
-    public Array intersect(Array other) {
-        var intersection = new Array(count);
-        for (int item : items) {
-            if (other.indexOf(item) >= 0) {
-                intersection.insert(item);
-            }
-        }
-        return intersection;
-    }
-    public void removeAt(int index) {
-        if (index < 0 || index >= count) {
-            throw new IllegalArgumentException();
-        }
-        for (int i = index; i < count; i++) {
-            items[i] = items[i + 1];
-        }
-        count--;
-    }
-    public int indexOf(int item) {
-        for (int i = 0; i < count; i++) {
-            if (items[i] == item) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public void print() {
-        for (int i = 0; i < count; i++) {
-            System.out.println(items[i]);
-        }
-    }
-}
+package sturtures;
 public class DynamicArray {
-    public static void main(String[] args) {
-        Array a1 = new Array(3);
-        a1.insert(10);
-        a1.insert(20);
-        a1.print();
-    }
+ public static void main(String[] args) {
+ String[][] states = new String[5][2];
+ states[0][0] = "Telangana";
+ states[0][1] = "Heyderabad";
+ states[1][0] = "Karnataka";
+ states[1][1] = "Bengaluru";
+ states[2][0] = "Andhra";
+ states[2][1] = "Amravathi";
+ states[3][0] = "Tamilnadu";
+ states[3][1] = "Chennai";
+ states[4][0] = "Kerala";
+ states[4][1] = "Thiruvananthapuram";
+     for (String[] state : states) {
+         StringBuilder sb = new StringBuilder();
+         for (int j = 0; j < state.length; j++) {
+             if (j == 0) {
+                 sb.append("The capitol of ");
+             } else {
+                 sb.append(" is ");
+             }           sb.append(state[j]);
+         }
+         System.out.println(sb);
+     }
+ }
 }
